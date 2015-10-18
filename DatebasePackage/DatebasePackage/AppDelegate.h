600d08ca361f34fec3ad4384371ba28665b231e2
hbbdsqd <hbbdsqd@163.com>
2015-10-18 13:21:48 +0800

    创建工程

---
+++ DatebasePackage/DatebasePackage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DatebasePackage
+//
+//  Created by Mac on 15/10/18.
+//  Copyright (c) 2015年 hbbdsqd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
